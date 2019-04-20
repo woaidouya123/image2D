878120e3af1ee6aa2122efcc5f143d2359124bcb
心叶 <yelloxing@gmail.com>
2019-04-20 21:52:09 +0800

    添加接口文件

--- src/export.js
+++ src/export.js
@@ -3,6 +3,7 @@ import image2D from './library/core';
 import tree from './library/layout/tree';
 import Matrix4 from './library/Matrix4/index';
 import animation from './library/tools/animation';
+import { formatColor } from './core/color';
 
 /**
  * 挂载方法
@@ -11,6 +12,7 @@ import animation from './library/tools/animation';
 image2D.treeLayout = tree;
 image2D.Matrix4 = Matrix4;
 image2D.animation = animation;
+image2D.formatColor = formatColor;
 
 export {
 
