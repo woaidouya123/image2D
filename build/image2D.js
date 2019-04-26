be0f92b6e03a435e19c383abc58a93da0aba855e
心叶 <yelloxing@gmail.com>
2019-04-26 11:58:47 +0800

    painter / config

--- build/image2D.js
+++ build/image2D.js
@@ -12,7 +12,7 @@
     * Copyright yelloxing
     * Released under the MIT license
     *
-    * Date:Thu Apr 25 2019 10:01:02 GMT+0800 (GMT+08:00)
+    * Date:Fri Apr 26 2019 11:57:10 GMT+0800 (GMT+08:00)
     */
 
 "use strict";
@@ -1230,8 +1230,47 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
         };
     };
 
+    // 获取原生的canvas画笔
+    var getPainter = function getPainter(target) {
+        if (target) {
+            if (target.constructor === image2D) target = target[0];
+
+            if (target && target.nodeName.toLowerCase() === 'canvas') {
+                return target.getContext("2d");
+            } else if (isNode(target)) throw new Error('Painter is not a function!');
+        }
+        throw new Error('Target empty!');
+    };
+
     // 加强版本的画笔
-    function painter() {}
+    function painter() {
+
+        var canvas = this[0];
+        var painter = getPainter(canvas);
+
+        // 画笔
+        var enhancePainter = {
+
+            // 属性设置或获取
+            "config": function config() {
+                if (arguments.length === 1) {
+                    if (_typeof(arguments[0]) !== 'object') return painter[arguments[0]];
+                    for (var key in arguments[0]) {
+                        painter[key] = arguments[0][key];
+                    }
+                } else if (arguments.length === 2) painter[arguments[0]] = arguments[1];
+                return enhancePainter;
+            }
+
+            /**
+             * 基础方法
+             * ---------------
+             */
+
+        };
+
+        return enhancePainter;
+    }
 
     image2D.extend({
 
--- src/library/canvas2D/painter.js
+++ src/library/canvas2D/painter.js
@@ -7,7 +7,7 @@ export let getPainter = function (target) {
         if (target.constructor === image2D) target = target[0];
 
         if (target && target.nodeName.toLowerCase() === 'canvas') {
-            return target[0].getContext("2d");
+            return target.getContext("2d");
         } else if (isNode(target)) throw new Error('Painter is not a function!');
     }
     throw new Error('Target empty!');
@@ -16,4 +16,30 @@ export let getPainter = function (target) {
 // 加强版本的画笔
 export default function () {
 
+    let canvas = this[0];
+    let painter = getPainter(canvas);
+
+    // 画笔
+    let enhancePainter = {
+
+        // 属性设置或获取
+        "config": function () {
+            if (arguments.length === 1) {
+                if (typeof arguments[0] !== 'object') return painter[arguments[0]];
+                for (let key in arguments[0])
+                    painter[key] = arguments[0][key];
+            } else if (arguments.length === 2) painter[arguments[0]] = arguments[1];
+            return enhancePainter;
+        },
+
+        /**
+         * 基础方法
+         * ---------------
+         */
+
+
+
+    };
+
+    return enhancePainter;
 };
