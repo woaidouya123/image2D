b11c2ac359ef6c8b96d61e76b5f54e5e61031d54
心叶 <yelloxing@gmail.com>
2019-05-20 23:23:31 +0800

    发布的时候添加打包后文件

--- build/image2D.js
+++ build/image2D.js
@@ -5,14 +5,14 @@
     *
     * author 心叶
     *
-    * version 0.0.5-dev
+    * version 0.0.6-dev
     *
     * build Thu Apr 11 2019
     *
     * Copyright yelloxing
     * Released under the MIT license
     *
-    * Date:Sat May 18 2019 22:03:10 GMT+0800 (GMT+08:00)
+    * Date:Mon May 20 2019 23:23:04 GMT+0800 (GMT+08:00)
     */
 
 "use strict";
