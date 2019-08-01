7f13b7b983bec3891aa8eb3a2b02bd3943bd28f2
心叶 <yelloxing@gmail.com>
2019-08-01 17:11:18 +0800

    文档主体

--- docs/src/containers/api/index.jsx
+++ docs/src/containers/api/index.jsx
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom';
 
 export default class Api extends React.Component {
     render() {
-        return (<div>
+        return (<div className='api'>
             <ul>
-                <li>
-                    <Link to='/api/how-to-use'>如何使用</Link>
-                    <Link to='/api/xhtml'>结点操作</Link>
-                    <Link to='/api/painter'>画笔</Link>
-                    <Link to='/api/calculate'>计算</Link>
-                    <Link to='/api/tool'>小工具</Link>
-                </li>
+                <li><Link to='/api/how-to-use'>如何使用</Link></li>
+                <li><Link to='/api/xhtml'>结点操作</Link></li>
+                <li><Link to='/api/painter'>画笔</Link></li>
+                <li><Link to='/api/calculate'>计算</Link></li>
+                <li><Link to='/api/tool'>小工具</Link></li>
             </ul>
             <div>
                 <Router></Router>
