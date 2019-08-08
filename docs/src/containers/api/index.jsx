b47ea481f9422987fe4de026c3c1688751d00377
心叶 <yelloxing@gmail.com>
2019-08-08 09:57:22 +0800

    api菜单添加二级点击

--- docs/src/containers/api/index.jsx
+++ docs/src/containers/api/index.jsx
@@ -7,32 +7,44 @@ export default class Api extends React.Component {
     componentDidMount() {
         $$('.topmenu-item').attr('active', 'no');
         $$('#api').attr('active', 'yes');
+
+        let apimenus = $$('.apimenu-item', document.getElementById('api-nav'));
+        for (let i = 0; i < apimenus.length; i++) {
+            let nodeName = apimenus[i].getAttribute('id');
+            let items = document.getElementsByName(nodeName);
+            for (let j = 0; j < items.length; j++) {
+                items[j].setAttribute('url', "#/api/" + nodeName);
+                items[j].setAttribute('index', j);
+                items[j].setAttribute('flag', 'api-item');
+            }
+        }
+
     }
     render() {
         return (<div className='api'>
             <ul id='api-nav'>
                 <li id='how-to-use' className='apimenu-item'><Link to='/api/how-to-use'>如何使用</Link></li>
-                <li className='level1 item' name='how-to-use-small'>关注的问题</li>
-                <li className='level1 item' name='how-to-use-small'>使用</li>
-                <li className='level1 item' name='how-to-use-small'>获取帮助</li>
+                <li className='item' name='how-to-use'>关注的问题</li>
+                <li className='item' name='how-to-use'>使用</li>
+                <li className='item' name='how-to-use'>获取帮助</li>
                 <li id='xhtml' className='apimenu-item'><Link to='/api/xhtml'>结点操作</Link></li>
-                <li className='level1 item' name='xhtml-small'>结点对象</li>
-                <li className='level1 item' name='xhtml-small'>编辑</li>
-                <li className='level1 item' name='xhtml-small'>样式和属性</li>
-                <li className='level1 item' name='xhtml-small'>事件相关</li>
-                <li className='level1 item' name='xhtml-small'>数据绑定</li>
+                <li className='item' name='xhtml'>结点对象</li>
+                <li className='item' name='xhtml'>编辑</li>
+                <li className='item' name='xhtml'>样式和属性</li>
+                <li className='item' name='xhtml'>事件相关</li>
+                <li className='item' name='xhtml'>数据绑定</li>
                 <li id='painter' className='apimenu-item'><Link to='/api/painter'>画笔</Link></li>
-                <li className='level1 item' name='painter-small'>位图canvas2D</li>
-                <li className='level1 item' name='painter-small'>矢图svg</li>
-                <li className='level1 item' name='painter-small'>通用绘图方法</li>
+                <li className='item' name='painter'>位图canvas2D</li>
+                <li className='item' name='painter'>矢图svg</li>
+                <li className='item' name='painter'>通用绘图方法</li>
                 <li id='calculate' className='apimenu-item'><Link to='/api/calculate'>计算</Link></li>
-                <li className='level1 item' name='calculate-small'>二维坐标变换</li>
-                <li className='level1 item' name='calculate-small'>矩阵坐标变换</li>
-                <li className='level1 item' name='calculate-small'>曲线插值</li>
-                <li className='level1 item' name='calculate-small'>布局</li>
-                <li className='level1 item' name='calculate-small'>动画轮询</li>
+                <li className='item' name='calculate'>二维坐标变换</li>
+                <li className='item' name='calculate'>矩阵坐标变换</li>
+                <li className='item' name='calculate'>曲线插值</li>
+                <li className='item' name='calculate'>布局</li>
+                <li className='item' name='calculate'>动画轮询</li>
                 <li id='tool' className='apimenu-item'><Link to='/api/tool'>小工具</Link></li>
-                <li className='level1 item' name='tool-small'>图层</li>
+                <li className='item' name='tool'>图层</li>
             </ul>
             <div>
                 <Router></Router>
