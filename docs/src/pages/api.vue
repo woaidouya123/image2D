55868ee72f2f680fa76aa2f979e400452cb08ec0
心叶 <yelloxing@gmail.com>
2019-11-21 23:56:41 +0800

    迁移接口文档部分内容

--- docs/src/pages/api.vue
+++ docs/src/pages/api.vue
@@ -1,18 +1,162 @@
 <template>
-  <div>
-
-  </div>
+    <div>
+        <ul id='api-nav'>
+            <li id='how-to-use' class='apimenu-item'><router-link to='/api/how-to-use'>如何使用</router-link></li>
+            <li class='item' name='how-to-use'>关注的问题</li>
+            <li class='item' name='how-to-use'>使用</li>
+            <li class='item' name='how-to-use'>获取帮助</li>
+            <li id='xhtml' class='apimenu-item'><router-link to='/api/xhtml'>结点操作</router-link></li>
+            <li class='item' name='xhtml'>结点对象</li>
+            <li class='item' name='xhtml'>编辑</li>
+            <li class='item' name='xhtml'>样式和属性</li>
+            <li class='item' name='xhtml'>事件相关</li>
+            <li class='item' name='xhtml'>数据绑定</li>
+            <li id='painter' class='apimenu-item'><router-link to='/api/painter'>画笔</router-link></li>
+            <li class='item' name='painter'>canvas2D</li>
+            <li class='item' name='painter'>svg</li>
+            <li class='item' name='painter'>绘图方法</li>
+            <li class='item' name='painter'>渐变色</li>
+            <li class='item' name='painter'>变换</li>
+            <li id='calculate' class='apimenu-item'><router-link to='/api/calculate'>计算</router-link></li>
+            <li class='item' name='calculate'>二维坐标变换</li>
+            <li class='item' name='calculate'>矩阵坐标变换</li>
+            <li class='item' name='calculate'>曲线插值</li>
+            <li class='item' name='calculate'>布局</li>
+            <li class='item' name='calculate'>动画轮询</li>
+            <li id='tool' class='apimenu-item'><router-link to='/api/tool'>补充</router-link></li>
+            <li class='item' name='tool'>图层</li>
+        </ul>
+        <div>
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 <script>
 import update from "../service/update";
 
 export default {
-  mounted() {
-    update("api");
-  }
+    methods:{
+        doScroll() {
+            let toTop = document.documentElement.scrollTop || document.body.scrollTop;
+
+            // 控制左边菜单位置
+            let apiNav = document.getElementById('api-nav');
+            if (toTop > 70)
+                apiNav.style.top = (toTop - 70) + 'px';
+            else
+                apiNav.style.top = '0px';
+
+        }
+    },
+    mounted() {
+        update("api");
+        window.addEventListener("scroll", this.doScroll);
+    },
+    destroyed() {
+        window.removeEventListener("scroll", this.doScroll);
+    }
 };
 </script>
 
 <style lang="scss" scoped>
+$navWidth:1.4rem;
+div {
+    font-size: 0;
+    &>* {
+        display: inline-block;
+        font-size: .16rem;
+        vertical-align: top;
+    }
+    &>ul {
+        width: $navWidth;
+        padding: .3rem .1rem;
+        position: relative;
+        max-height: calc(100vh - 2rem);
+        overflow-y: scroll;
+        &>li {
+            color: rgb(167, 160, 160);
+            &>a {
+                width: 100%;
+                display: inline-block;
+                text-align: left;
+                line-height: 2em;
+                font-size: .14rem;
+                color: inherit;
+            }
+            &[active='yes']>a {
+                color: rgb(113, 30, 50);
+            }
+            &.item {
+                cursor: pointer;
+                line-height: 1.8em;
+                &[active='yes'] {
+                    color: rgb(113, 30, 50);
+                }
+                padding-left: 1em;
+                font-size: .13rem;
+            }
+        }
+    }
+    &>div {
+        width: calc(100% - #{$navWidth});
+        padding: .1rem;
+        /deep/ .container {
+            width: 100%;
+            line-height: 2em;
+            &>pre {
+                font-size: .14rem;
+            }
+            &>.title {
+                font-weight: 800;
+                line-height: 2em;
+                padding: .4rem 0 .2rem;
+                margin-top: -.3rem;
+                &.big {
+                    font-size: .18rem;
+                    color: rgb(203, 83, 139);
+                }
+                &.small {
+                    font-size: .16rem;
+                    color: rgb(203, 83, 139);
+                }
+                &.little {
+                    font-size: .14rem;
+                    color: #85a646;
+                }
+                &.sub-little {
+                    font-size: .12rem;
+                    color: #f00;
+                }
+            }
+            &>p {
+                text-indent: 2em;
+                font-size: .14rem;
+                font-weight: 600;
+                &>a {
+                    text-indent: 0;
+                    color: #ac2c23;
+                    text-decoration: underline;
+                }
+                &.warn {
+                    background-image: url('../assets/warn.png');
+                    background-repeat: no-repeat;
+                    background-size: auto .21rem;
+                    font-size: .12rem;
+                    color: #dac305;
+                    font-weight: 400;
+                }
+            }
+            &>ul {
+                margin-left: 5em;
+                font-size: .13rem;
+                line-height: 2.4em;
+                &>li {
+                    list-style-type: decimal;
+                }
+            }
+        }
+    }
+}
+
 </style>
--- docs/src/router.js
+++ docs/src/router.js
@@ -10,16 +10,33 @@ const router = new VueRouter({
         component: resolve => require(['./pages/guide.vue'], resolve)
     }, {
         path: '/api',
-        component: resolve => require(['./pages/api.vue'], resolve)
+        component: resolve => require(['./pages/api.vue'], resolve),
+        children: [{
+            path: 'how-to-use',
+            component: resolve => require(['./pages/api/how-to-use.vue'], resolve)
+        }, {
+            path: 'xhtml',
+            component: resolve => require(['./pages/api/xhtml.vue'], resolve)
+        }, {
+            path: 'painter',
+            component: resolve => require(['./pages/api/painter.vue'], resolve)
+        }, {
+            path: 'calculate',
+            component: resolve => require(['./pages/api/calculate.vue'], resolve)
+        }, {
+            path: 'tool',
+            component: resolve => require(['./pages/api/tool.vue'], resolve)
+        }, {
+            path: '*',
+            redirect: 'how-to-use'
+        }]
     }, {
         path: '/about',
         component: resolve => require(['./pages/about.vue'], resolve)
-    },
-    {
+    }, {
         path: '/*',
         redirect: 'guide'
-    }
-    ]
+    }]
 });
 
 export default router;
