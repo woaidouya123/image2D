30bd69361b6c9be55ea617b4fb3d4530d3f81226
心叶 <yelloxing@gmail.com>
2019-11-23 00:56:03 +0800

    优化打包（cuf调度）

--- build/image2D.js
+++ build/image2D.js
@@ -11,7 +11,7 @@
 * Copyright yelloxing
 * Released under the MIT license
 *
-* Date:Fri Nov 22 2019 23:29:26 GMT+0800 (GMT+08:00)
+* Date:Sat Nov 23 2019 00:54:47 GMT+0800 (GMT+08:00)
 */
 
 'use strict';
