24f007965d8ed6f89a4f51ed2a5ea9dcc5be2b40
心叶 <yelloxing@gmail.com>
2020-03-26 14:49:31 +0800

    -

--- docs/src/entry.js
+++ docs/src/entry.js
@@ -3,11 +3,12 @@ import iCrush from 'icrush';
 // 引入启动界面
 import App from './App.iCrush';
 
-// 引入基础样式
+// 引入样式
 import '@yelloxing/normalize.css';
+import './styles/style.scss';
 
 // 删除提示内容
-document.getElementById('root').innerHTML = "<!-- image2D:https://github.com/yelloxing/image2D -->";
+document.getElementById('root').innerHTML = "<!-- image2D: https://github.com/yelloxing/image2D -->";
 
 //根对象
 window.icrush = new iCrush({
